8c8905a32d97fc3f5785b689665f2100cc584af8
Scott Ferguson <scottwferg@gmail.com>
2013-04-05 16:29:18 -0500

    Patch to fix is_string crash

--- piston/resource.py
+++ piston/resource.py
@@ -72,7 +72,7 @@ class Resource(object):
         `Resource` subclass.
         """
         resp = rc.BAD_REQUEST
-        resp.write(' '+str(e.form.errors))
+        resp.write(' '+unicode(e.form.errors))
         return resp
 
     @property
@@ -181,7 +181,7 @@ class Resource(object):
         # If we're looking at a response object which contains non-string
         # content, then assume we should use the emitter to format that 
         # content
-        if isinstance(result, HttpResponse) and not result._is_string:
+        if isinstance(result, HttpResponse) and not isinstance(result, str):
             status_code = result.status_code
             # Note: We can't use result.content here because that method attempts
             # to convert the content into a string which we don't want. 
